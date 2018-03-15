c954177fe51372d5e88436c79ff1e83353f397d1
catalin <catalin@DESKTOP>
2018-03-15 10:40:34 +0200

    97

--- ProjectEuler/Problem079.cs
+++ ProjectEuler/Problem079.cs
@@ -1,31 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ProjectEuler
 {
     class Problem079 : IProblem
     {
-        struct Digit
+        Dictionary<byte, Digit> _digits = new Dictionary<byte, Digit>();
+
+        class Digit
         {
-            internal List<byte> Left;
-            internal List<byte> Right;
-            public Digit(byte left, byte right)
+            readonly byte _value;
+
+            internal byte Value { get { return _value; } }
+            internal HashSet<Digit> Left { get; private set; }
+            internal HashSet<Digit> Right { get; private set; }
+            internal int Count { get { return Left.Count + Right.Count; } }
+
+            public Digit(byte value)
             {
-                Left = new List<byte> { left };
-                Right = new List<byte> { right };
+                _value = value;
+                Left = new HashSet<Digit>();
+                Right = new HashSet<Digit>();
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}: Left({1}) Right({2})", _value, string.Join(", ", Left.Select(v => v.Value.ToString()).ToArray()), string.Join(", ", Right.Select(v => v.Value.ToString()).ToArray()));
+            }
+        }
+
+        void Add(Digit d)
+        {
+            foreach (var l in d.Left)
+            {
+                foreach (var ll in l.Left)
+                    if (!d.Left.Contains(ll))
+                        d.Left.Add(ll);
+            }
+            foreach(var r in d.Right)
+            {
+                foreach (var rr in r.Right)
+                    if (!d.Right.Contains(rr))
+                        d.Right.Add(rr);
+            }
+        }
+
+        Digit GetOrAdd(byte d)
+        {
+            if (_digits.ContainsKey(d))
+                return _digits[d];
+            else
+            {
+                var dig = new Digit(d);
+                _digits.Add(d, dig);
+                return dig;
             }
         }
 
         public string GetResult()
         {
-            Dictionary<byte, Digit> digits = new Dictionary<byte, Digit>();
-            using(StreamReader sr = new StreamReader("Problem079.txt"))
+            using (StreamReader sr = new StreamReader("Problem079.txt"))
             {
                 while (!sr.EndOfStream)
                 {
-                    string s = sr.ReadLine();
+                    var s = sr.ReadLine().ToCharArray().Select(c => GetOrAdd(byte.Parse(c.ToString()))).ToArray();
+                    s[1].Left.Add(s[0]);
+                    s[1].Right.Add(s[2]);
                 }
             }
+
+            var d = _digits.Aggregate((i, j) => i.Value.Count > j.Value.Count ? i : j).Value;
+            Add(d);
+            foreach (var l in d.Left)
+                Add(l);
+            foreach (var r in d.Right)
+                Add(r);
+
+            string result = "";
 
             return "";
         }
---
+++ ProjectEuler/Problem097.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectEuler
+{
+    class Problem097 : IProblem
+    {
+        public string GetResult()
+        {
+            const int pow = 7830457;
+            const long dig = 10000000000;
+            long num = 28433;
+            for (int i = 0; i < pow; i++)
+                num = num * 2 % dig;
+            num = (num + 1) % dig;
+
+            return num.ToString();
+        }
+    }
+}
