c5d7c540988f7902cdcfe05157177706bfc234f6
catalin <catalin@DESKTOP>
2018-04-17 21:10:18 +0300

    80

---
+++ ProjectEuler/051-075/Problem072.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectEuler
+{
+    class Problem072 : IProblem
+    {
+        public string GetResult()
+        {
+            const int limit = 1000000;
+            long count = 0;
+            for (int n = 2; n <= limit; n++)
+                count += Common.Totient(n, 2, n, Common.GeneratePrimes(limit));
+
+            return count.ToString();
+        }
+    }
+}
---
+++ ProjectEuler/076-100/Problem080.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Problem080 : IProblem
+    {
+        public string GetResult()
+        {
+            int sum = 0;
+            for (int n = 2; n < 100; n++)
+            {
+                double s = Math.Sqrt(n);
+                for (int i = 0; i < 100; i++)
+                {
+                    s = (s - (int)s) * 10;
+                    int j = (int)s;
+                    sum += j;
+                    s -= j;
+                }
+            }
+
+            return sum.ToString();
+        }
+    }
+}
