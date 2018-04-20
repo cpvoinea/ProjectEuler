cc28a4c52b2bc7edafadda629c2ed53ba6aa6818
catalin <catalin@DESKTOP>
2018-04-20 23:22:58 +0300

    t

---
+++ ProjectEuler/076-100/Problem078.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    class Problem078 : IProblem
+    {
+        const int limit = 1000000;
+        Dictionary<long, long> counts = new Dictionary<long, long>();
+
+        long Comb(long n, long m)
+        {
+            if (m == n)
+                return 1;
+            if (m > n)
+                return 0;
+            long key = n * 10000 + m;
+            if (counts.ContainsKey(key))
+                return counts[key];
+
+            long s = 1;
+            for (long i = 2; i <= m; i++)
+                s = (s + Comb(n - m, i)) % limit;
+            s = s % limit;
+            counts[key] = s;
+            return s;
+        }
+
+        public string GetResult()
+        {
+            long n = 2;
+            while (n < 10000)
+            {
+                long s = 1;
+                for (long i = 2; i <= n; i++)
+                    s = (s + Comb(n, i)) % limit;
+                s = s % limit;
+                if (s == 0)
+                    return n.ToString();
+                n++;
+            }
+
+            return "N/A";
+        }
+    }
+}
