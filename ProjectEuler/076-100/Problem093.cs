2e2973f755c73845f80e019347db9d0c8804c0ae
Catalin Voinea <cpvoinea@gmail.com>
2018-05-31 17:49:44 +0300

    93 2

--- ProjectEuler/076-100/Problem093.cs
+++ ProjectEuler/076-100/Problem093.cs
@@ -31,6 +31,36 @@ namespace ProjectEuler
                                     {
                                         result.Add(new[] { c[i], c[j], c[k], c[l] });
                                         result.Add(new[] { c[i], c[j], c[l], c[k] });
+                                        result.Add(new[] { -c[i], c[j], c[k], c[l] });
+                                        result.Add(new[] { -c[i], c[j], c[l], c[k] });
+                                        result.Add(new[] { c[i], -c[j], c[k], c[l] });
+                                        result.Add(new[] { c[i], -c[j], c[l], c[k] });
+                                        result.Add(new[] { c[i], c[j], -c[k], c[l] });
+                                        result.Add(new[] { c[i], c[j], -c[l], c[k] });
+                                        result.Add(new[] { c[i], c[j], c[k], -c[l] });
+                                        result.Add(new[] { c[i], c[j], c[l], -c[k] });
+                                        result.Add(new[] { -c[i], -c[j], c[k], c[l] });
+                                        result.Add(new[] { -c[i], -c[j], c[l], c[k] });
+                                        result.Add(new[] { -c[i], c[j], -c[k], c[l] });
+                                        result.Add(new[] { -c[i], c[j], -c[l], c[k] });
+                                        result.Add(new[] { -c[i], c[j], c[k], -c[l] });
+                                        result.Add(new[] { -c[i], c[j], c[l], -c[k] });
+                                        result.Add(new[] { c[i], -c[j], -c[k], c[l] });
+                                        result.Add(new[] { c[i], -c[j], -c[l], c[k] });
+                                        result.Add(new[] { c[i], -c[j], c[k], -c[l] });
+                                        result.Add(new[] { c[i], -c[j], c[l], -c[k] });
+                                        result.Add(new[] { c[i], c[j], -c[k], -c[l] });
+                                        result.Add(new[] { c[i], c[j], -c[l], -c[k] });
+                                        result.Add(new[] { -c[i], -c[j], -c[k], c[l] });
+                                        result.Add(new[] { -c[i], -c[j], -c[l], c[k] });
+                                        result.Add(new[] { -c[i], -c[j], c[k], -c[l] });
+                                        result.Add(new[] { -c[i], -c[j], c[l], -c[k] });
+                                        result.Add(new[] { -c[i], c[j], -c[k], -c[l] });
+                                        result.Add(new[] { -c[i], c[j], -c[l], -c[k] });
+                                        result.Add(new[] { c[i], -c[j], -c[k], -c[l] });
+                                        result.Add(new[] { c[i], -c[j], -c[l], -c[k] });
+                                        result.Add(new[] { -c[i], -c[j], -c[k], -c[l] });
+                                        result.Add(new[] { -c[i], -c[j], -c[l], -c[k] });
                                     }
             return result.ToArray();
         }
@@ -42,7 +72,9 @@ namespace ProjectEuler
                 for (int j = i + 1; j <= 7; j++)
                     for (int k = j + 1; k <= 8; k++)
                         for (int l = k + 1; l <= 9; l++)
+                        {
                             result.Add(new[] { i, j, k, l });
+                        }
             return result.ToArray();
         }
 
@@ -51,10 +83,10 @@ namespace ProjectEuler
             switch (par)
             {
                 case 0:
-                    if (o2 > 1)
+                    if (o2 > 0)
                     {
                         double e = Oper(p[1], p[2], o2);
-                        if (o3 > 1)
+                        if (o3 > 0)
                             return Oper(p[0], Oper(e, p[3], o3), o1);
                         else
                             return Oper(Oper(p[0], e, o1), p[2], o3);
@@ -62,7 +94,7 @@ namespace ProjectEuler
                     else
                     {
                         double e = Oper(p[0], p[1], o1);
-                        if (o3 > 1)
+                        if (o3 > 0)
                             return Oper(e, Oper(p[2], p[3], o3), o2);
                         else
                             return Oper(Oper(e, p[2], o2), p[3], o3);
@@ -70,27 +102,27 @@ namespace ProjectEuler
                 case 1:
                     return Oper(Oper(p[0], p[1], o1), Oper(p[2], p[3], o3), o2);
                 case 2:
-                    if (o3 > 1)
+                    if (o3 > 0)
                         return Oper(Oper(p[0], p[1], o1), Oper(p[2], p[3], o3), o2);
                     else
                         return Oper(Oper(Oper(p[0], p[1], o1), p[2], o2), p[3], o3);
                 case 3:
-                    if (o2 > 1)
+                    if (o2 > 0)
                         return Oper(p[0], Oper(p[1], Oper(p[2], p[3], o3), o2), o1);
                     else
                         return Oper(Oper(p[0], p[1], o1), Oper(p[2], p[3], o3), o2);
                 case 4:
-                    if (o3 > 1)
+                    if (o3 > 0)
                         return Oper(p[0], Oper(Oper(p[1], p[2], o2), p[3], o3), o1);
                     else
                         return Oper(Oper(p[0], Oper(p[1], p[2], o2), o1), p[3], o3);
                 case 5:
-                    if (o2 > 1)
+                    if (o2 > 0)
                         return Oper(Oper(p[0], Oper(p[1], p[2], o2), o1), p[3], o3);
                     else
                         return Oper(Oper(Oper(p[0], p[1], o1), p[2], o2), p[3], o3);
                 case 6:
-                    if (o3 > 1)
+                    if (o3 > 0)
                         return Oper(p[0], Oper(p[1], Oper(p[2], p[3], o3), o2), o1);
                     else
                         return Oper(p[0], Oper(Oper(p[1], p[2], o2), p[3], o3), o1);
@@ -109,24 +141,20 @@ namespace ProjectEuler
                 int[][] perm = GetPerm(c);
                 foreach (int[] p in perm)
                 {
-                    for (int o1 = 0; o1 < 4; o1++)
-                        for (int o2 = 0; o2 < 4; o2++)
-                            for (int o3 = 0; o3 < 4; o3++)
+                    for (int o1 = 0; o1 < 3; o1++)
+                        for (int o2 = 0; o2 < 3; o2++)
+                            for (int o3 = 0; o3 < 3; o3++)
                                 for (int par = 0; par < 7; par++)
                                 {
                                     double e = Evaluate(p, o1, o2, o3, par);
-                                    if (e > 0 && e - (int)e == 0)
-                                        b[(int)e] = true;
-
-                                    e = Evaluate(new[] { -p[0], p[1], p[2], p[3] }, o1, o2, o3, par);
-                                    if (e > 0 && e - (int)e == 0)
+                                    if (e > 0 && e - (int)e < 0.001 && !b[(int)e])
                                         b[(int)e] = true;
                                 }
                 }
 
                 int i = 1;
                 while (b[i++]) ;
-                if(i > max)
+                if (i > max)
                 {
                     max = i;
                     Array.Sort(c);
