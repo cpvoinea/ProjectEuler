3951b1a709ded132986b2f537381bee9146931e7
Catalin Voinea <cpvoinea@gmail.com>
2018-06-08 18:33:47 +0300

    191

---
+++ ProjectEuler/101-200/Problem191.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectEuler
+{
+    class Problem191 : IProblem
+    {
+        const int n = 30;
+        long count = 0;
+
+        void CountFrom(int i, bool wasLate, int absences)
+        {
+            if (i >= n)
+                count++;
+            else
+            {
+                if (!wasLate)
+                    CountFrom(i + 1, true, 0);
+                if (absences < 2)
+                    CountFrom(i + 1, wasLate, absences + 1);
+                CountFrom(i + 1, wasLate, 0);
+            }
+        }
+
+        public string GetResult()
+        {
+            CountFrom(0, false, 0);
+            return count.ToString();
+        }
+    }
+}
