6e385c1b262c19f353d1fc02460a1484f546d1e5
Catalin Voinea <cpvoinea@gmail.com>
2018-06-08 12:56:42 +0300

    88

--- ProjectEuler/076-100/Problem088.cs
+++ ProjectEuler/076-100/Problem088.cs
@@ -4,44 +4,59 @@ namespace ProjectEuler
 {
     class Problem088 : IProblem
     {
+        const int limit = 12000;
+        readonly int[] min = new int[limit + 1];
+
+        int GetSum(int[] v)
+        {
+            int s = 0;
+            for (int i = 0; i < v.Length; i++)
+                s += v[i];
+            return s;
+        }
+
+        int GetProd(int[] v)
+        {
+            int p = 1;
+            for (int i = 0; i < v.Length; i++)
+                p *= v[i];
+            return p;
+        }
+
+        bool CheckFactors(int[] f)
+        {
+            int p = GetProd(f);
+            if (p > 2 * limit)
+                return false;
+            int s = GetSum(f);
+            int k = p - s + f.Length;
+            if (k <= limit && k >= 2 && (min[k] == 0 || p < min[k]))
+                min[k] = p;
+            return true;
+        }
+
+        bool IterateFactors(int[] f, int i, int p)
+        {
+            if (p > 2 * limit)
+                return false;
+            if (i >= f.Length)
+                return CheckFactors(f);
+            else
+                for (int n = 2; n <= limit; n++)
+                {
+                    f[i] = n;
+                    if (!IterateFactors(f, i + 1, p * n))
+                        return true;
+                }
+            return true;
+        }
+
         public string GetResult()
         {
-            const int limit = 12000;
-            int[] min = new int[limit + 1];
-            int[] factors = new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
-
-            int count = 13;
-            while (count > 0)
+            for (int i = 2; i <= 14; i++)
             {
-                int p = 1;
-                for (int i = 0; i <= count; i++)
-                    p *= factors[i];
-                if (count == 0 && p > 2 * limit)
-                    break;
-                if (p > 2 * limit)
-                {
-                    p /= factors[count] * factors[count - 1];
-                    factors[count - 1]++;
-                    factors[count] = factors[count - 1];
-                    p *= factors[count] * factors[count - 1];
-                    if (p > 2 * limit)
-                    {
-                        p /= factors[count] * factors[count - 1];
-                        factors[count] = 0;
-                        count--;
-                        factors[count] = 2;
-                        p *= factors[count];
-                    }
-                }
-
-                int s = 0;
-                for (int i = 0; i <= count; i++)
-                    s += factors[i];
-                int k = p - s + count + 1;
-                if (k >= 2 && k <= limit && (min[k] == 0 || p < min[k]))
-                    min[k] = p;
-
-                factors[count]++;
+                int[] f = new int[i];
+                IterateFactors(f, 0, 1);
             }
 
             HashSet<int> vals = new HashSet<int>();
@@ -55,6 +70,7 @@ namespace ProjectEuler
                     vals.Add(n);
                 }
             }
+
             return sum.ToString();
         }
     }
