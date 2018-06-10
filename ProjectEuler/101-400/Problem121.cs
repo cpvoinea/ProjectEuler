b1a24162fdd1a4310b9237a54eecf23cd3411d8d
catalin <catalin@DESKTOP>
2018-06-10 13:32:27 +0300

    121

---
+++ ProjectEuler/101-400/Problem121.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectEuler
+{
+    class Problem121 : IProblem
+    {
+        const int limit = 15;
+
+        long Count(int turns, int draw)
+        {
+            if (turns == limit)
+                return draw > limit / 2 ? 1 : 0;
+            turns++;
+            // there are (turns+1) balls on each turn, 1 of them is blue
+            return Count(turns, draw + 1) + turns * Count(turns, draw);
+        }
+
+        public string GetResult()
+        {
+            long wins = Count(0, 0);
+            long count = 1;
+            for (int i = 2; i <= limit + 1; i++)
+                count *= i;
+            long sum = count / wins;
+
+            System.Console.WriteLine("{0} / {1}", wins, count);
+
+            return sum.ToString();
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -12,7 +12,7 @@ namespace ProjectEuler
 		{
 			IProblem p;
 
-			p = new Problem191();
+			p = new Problem121();
 			Console.WriteLine("Result = " + p.GetResult());
 		
 			Console.ReadKey();
