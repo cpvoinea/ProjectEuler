ccfb00f52f3ed5e76f5d8d4b156368e544870962
cpvoinea <cpvoinea@gmail.com>
2020-06-22 12:59:58 +0300

    692

---
+++ 601+/Problem692.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectEuler
+{
+    class Problem692 : IProblem
+    {
+        public object GetResult()
+        {
+            const long M = 23416728348467685;
+            long f1 = 1, f2 = 2;
+            long g1 = 1, g2 = 3;
+            while (f2 < M)
+            {
+                f2 = f1 + f2;
+                f1 = f2 - f1;
+                long g3 = g1 + g2 + f1;
+                g1 = g2;
+                g2 = g3;
+            }
+
+            return g2;
+        }
+    }
+}
