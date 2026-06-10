df18bc2a66a1a2bb1bf25dc0670bee64d293131e
cpvoinea <cpvoinea@gmail.com>
2026-06-10 13:06:42 +0300

    Add files via upload

---
+++ 800+/Problem872.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    public class Problem872 : IProblem
+    {
+        public object GetResult()
+        {
+            long n = 100_000_000_000_000_000;
+            long k = 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9;
+            k = k * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9;
+            long d = n - k;
+            var sn = new string(Convert.ToString(d, 2).Reverse().ToArray());
+            long s = n;
+            long p = 1;
+            for (int i = 0; i < sn.Length; i++)
+            {
+                if (sn[i] - '0' == 1)
+                {
+                    n = n - p;
+                    s += n;
+                }
+                p *= 2;
+            }
+
+            return s;
+        }
+    }
+}
