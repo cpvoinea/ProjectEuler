45d08617ea1167351a2d3f43e0f53a1515f14a9f
cpvoinea <cpvoinea@gmail.com>
2026-07-03 17:41:19 +0300

    Add files via upload

---
+++ 800+/Problem918.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public class Problem918 : IProblem
+    {
+        private long A(long n)
+        {
+            if (n <= 1)
+                return 1;
+
+            long m = n;
+            long p = 1;
+            while (n % 2 == 0)
+            {
+                p *= 2;
+                n /= 2;
+            }
+            if (n <= 1)
+                return p;
+
+            var r = p * (A(n / 2) - 3 * A(n / 2 + 1));
+            return r;
+        }
+
+        public object GetResult()
+        {
+            const long N = 1_000_000_000_000;
+            long s = A(N) + A(1) + A(2) + A(3);
+            long k = (N - 1) / 4;
+            s += 6 * (1 - A(k + 1));
+
+            return s;
+        }
+    }
+}
